c3f93a244575569329e1b793acb16513d2aa1b13
Riza Barone <ysa_e@hotmail.com>
2015-11-01 08:20:36 -0500

    modified js code

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -1,18 +1,17 @@
-(function executeThis() {
+$(document).ready(function() {
 
-    function FizzBuzz(numCount) {
-        if (numCount % 15 == 0) {
-            document.write("FizzBuzz ");
-        } else if (numCount % 3 == 0) {
-            document.write("Fizz ");
-        } else if (numCount % 5 == 0) {
-            document.write("Buzz ");
-        } else {
-            document.write(numCount + ' ');
-        }
-    }
-
-    for (var numCount = 1; numCount < 100; numCount++) {
-        FizzBuzz(numCount);
-    }
-}());
+	for (var numCount = 1; numCount < 100; numCount++) {
+		var display = numCount;
+		if (numCount % 3 == 0){
+			display = "Fizz";
+		}
+		if (numCount % 5 == 0) { 
+			display = "Buzz";
+		}        
+		if (numCount  % 3 == 0 && numCount % 5 == 0)  { 
+			display = "FizzBuzz";
+		}
+		console.log(display);
+	}
+		
+});

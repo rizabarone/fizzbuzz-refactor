7d9f8c7cf7ca85f31b3f29dc051d8319a4728d05
Riza Barone <ysa_e@hotmail.com>
2015-11-06 20:13:15 -0500

    added other functions

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -1,20 +1,27 @@
 
+    var maxNumber = parseInt(prompt("Type any number from 1 to 100", ""));
+            
+        
     (function executeThis() {
 
     function FizzBuzz(numCount) {
         if (numCount % 15 == 0) {
-            document.write("FizzBuzz ");
+            document.write("FizzBuzz");
         } else if (numCount % 3 == 0) {
-            document.write("Fizz ");
+            document.write ("Fizz");
         } else if (numCount % 5 == 0) {
-            document.write("Buzz ");
+            document.write("Buzz");
         } else {
             document.write(numCount + ' ');
         }
+
+        return numCount;
     }
 
-    for (var numCount = 1; numCount < 100; numCount++) {
+    for (var numCount = 1; numCount <= maxNumber; numCount++) {
         FizzBuzz(numCount);
     }
+
+
 }());
 
